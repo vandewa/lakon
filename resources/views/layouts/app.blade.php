be6bebf36facd8b5f26f387cee2c0dce0e3bdd4e
Devan Dewananta <devandewananta@gmail.com>
2023-05-25 11:30:36 +0700

    biar select to jalan

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,9 +31,8 @@
   <link href="{{ asset('snacked/ltr/assets/css/header-colors.css')}}" rel="stylesheet" />
 
   <!-- Styles -->
-  {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" /> --}}
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
   <!-- Or for RTL support -->
 
   <!-- Scripts -->
