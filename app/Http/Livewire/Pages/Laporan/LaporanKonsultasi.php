f8159d351680559d819096b973d039bd1ee31501
damar wisnu candra prabowo <damar.wisnu1@gmail.com>
2023-06-06 16:45:28 +0700

    pemberian filter

--- app/Http/Livewire/Pages/Laporan/LaporanKonsultasi.php
+++ app/Http/Livewire/Pages/Laporan/LaporanKonsultasi.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Livewire\Pages\Laporan;
 
+use App\Models\ComCode;
 use App\Models\Tiket;
+use App\Models\User;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -10,11 +12,35 @@ class LaporanKonsultasi extends Component
 {
     use WithPagination;
 
+    public $status, $listOpd, $pilihStatus, $pilihOpd, $tanggalStart, $tanggalEnd;
+
     protected $paginationTheme = 'bootstrap';
+
+    public function mount() 
+    {
+        $this->status = ComCode::where('code_group', 'TIKET_ST')->get();
+        $this->listOpd = User::select('opd')->distinct()->limit(10)->get();
+    }
 
     public function render()
     {
-        $data = Tiket::with(['penanya'])->paginate(10);
+        $data = Tiket::with(['penanya']);
+
+        if($this->pilihStatus){
+            $data->where('tiket_st', $this->pilihStatus);
+        }
+
+        if($this->pilihOpd) {
+            $data->whereHas('penanya', function($a){
+                $a->where('opd', $this->pilihOpd);
+            });
+        }
+
+        if($this->tanggalStart && $this->tanggalEnd){
+            $data->whereBetween('created_at', [$this->tanggalStart, $this->tanggalEnd]);
+        }
+
+        $data = $data->paginate(10);
         return view('livewire.pages.laporan.laporan-konsultasi',[
             "items" => $data
         ]);
--- resources/views/livewire/pages/laporan/laporan-konsultasi.blade.php
+++ resources/views/livewire/pages/laporan/laporan-konsultasi.blade.php
@@ -13,6 +13,85 @@
         </nav>
         </div>
     </div>
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-4 row mb-3">
+                    <div class="col-md-3">
+                        <div class="col-form-label">
+                            Status
+                        </div>
+                    </div>
+                    <div class="col-md-9">
+                        <select name="" id="" class="form-control" wire:model="pilihStatus">
+                            <option value="">Pilih Status</option>
+                            @foreach ($status as $item)
+                                <option value={{ $item->code_cd }}> {{ $item->code_nm }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class="col-md-4 row mb-3">
+                    <div class="col-md-3">
+                        <div class="col-form-label">
+                            OPD
+                        </div>
+                    </div>
+                    <div class="col-md-9">
+                        <select name="" id="" class="form-control" wire:model="pilihOpd">
+                            <option value="">Pilih OPD</option>
+                            @foreach ($listOpd as $a)
+                                <option value={{ $a->opd }}> {{ $a->opd }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class="col-md-4 row mb-3">
+                    <div class="col-md-3">
+                        <div class="col-form-label">
+                            Urusan
+                        </div>
+                    </div>
+                    <div class="col-md-9">
+                        <select name="" id="" class="form-control">
+                            <option value="">Pilih Status</option>
+                            @foreach ($status as $item)
+                                <option value={{ $item->code_cd }}> {{ $item->code_nm }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+              
+                <div class="col-md-4 row mb-3">
+                    <div class="col-md-3">
+                        <div class="col-form-label">
+                            Tanggal Mulai
+                        </div>
+                    </div>
+                    <div class="col-md-9">
+                        <input type="date" class="form-control" wire:model='tanggalStart'>
+                    </div>
+                </div>
+                <div class="col-md-4 row mb-3">
+                    <div class="col-md-3">
+                        <div class="col-form-label">
+                            Tanggal Sampai
+                        </div>
+                    </div>
+                    <div class="col-md-9">
+                        <input type="date" class="form-control" wire:model="tanggalEnd">
+                    </div>
+                </div>
+                
+            </div>
+            <div class="float-end">
+                <button class="btn btn-primary">Export Excel</button>
+            </div>
+            
+        </div>
+        
+       
+    </div>
     <!--end breadcrumb-->
     <div class="card">
         <div class="card-body">
@@ -22,22 +101,22 @@
           <table class="table">
             <thead><tr>
                 <td>Kode Tiket</td>
+                <td>Tanggal Masuk</td>
                 <td>Subject</td>
                 <td>Pertanyaan</td>
                 <td>Penanya</td>
                 <td>Jawaban</td>
-                <td>Tanggal Pembuatan</td>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($items as $item)
                     <tr>
                         <td> {{ $item->kode_tiket }} </td>
+                        <td>{{ $item->created_at }}</td>
                         <td> {{ $item->subject }} </td>
                         <td> {!! $item->pertanyaan !!} </td>
-                        <td> {{ $item->penanya->name??"-" }}</td>
+                        <td> {{ $item->penanya->opd??"-" }}</td>
                         <td>{!! $item->jawaban !!}</td>
-                        <td>{{ $item->created_at }}</td>
                     </tr>
                 @endforeach
             </tbody>
