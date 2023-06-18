80deedb4eef52118db57f39b74283aa4707dbca9
vandewa <devandewananta@gmail.com>
2023-06-18 12:34:44 +0700

    href kembali

--- resources/views/livewire/pages/regulasi/page-regulasi.blade.php
+++ resources/views/livewire/pages/regulasi/page-regulasi.blade.php
@@ -52,7 +52,7 @@
                                 <div class="row">
                                     <label class="col-sm-3 col-form-label"></label>
                                     <div class="col-sm-12 text-end">
-                                        <a href="{{ route('irban.index') }}" class="px-5 btn btn-secondary">Kembali</a>
+                                        <a href="{{ route('regulasi.index') }}" class="px-5 btn btn-secondary">Kembali</a>
                                         <button type="submit" class="px-5 btn btn-primary">Simpan</button>
                                     </div>
                                 </div>
