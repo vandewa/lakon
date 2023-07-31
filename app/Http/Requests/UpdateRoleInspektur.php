d7137775a8d08945a648591d9380cfad5b5df420
Devan Dewananta <devandewananta@gmail.com>
2023-07-31 12:24:31 +0700

    tambah validasi inspektur

---
+++ app/Http/Requests/UpdateRoleInspektur.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRoleInspektur extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'tiket_status_st' => 'required',
+            'keterangan' => 'required',
+        ];
+    }
+}
